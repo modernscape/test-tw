ab9a8ae058a5eafde2fa9bd4f9c2b4058ea61a57
modernscape <modernscape@gmail.com>
2026-01-19 21:00:41 +0900

    next.config.ts修正

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    // @ts-expect-error: Next.jsの型定義が追いついていないための暫定対応
+    reactCompiler: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
